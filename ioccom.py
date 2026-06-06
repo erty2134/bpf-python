dbe9a357895c24324ff537d27a543a6e3ebed9a1
erty2134 <olly.lansdown@gmail.com>
2026-06-06 10:53:42 +1000

    restructure

--- ioccom.py
+++ ioccom.py
@@ -1,59 +1,60 @@
 import ctypes
 
 
+type __uint32_t = ctypes.c_uint32
+
+
 # Ioctl's have the command encoded in the lower word, and the size of
 # any in or out parameters in the upper word.  The high 3 bits of the
 # upper word are used to encode the in/out status of the parameter.
 
-type __uint32_t = ctypes.c_uint32
+IOCPARM_MASK: int = __uint32_t(0x1fff) # parameter length, at most 13 bits
 
-IOCPARM_MASK: int = ctypes.c_uint32(0x1fff) # parameter length, at most 13 bits
+def iocparm_len(x: int) -> __uint32_t:
+    return __uint32_t(((x) >> 16) & IOCPARM_MASK)
 
-def iocparm_len(x: int) -> ctypes.c_uint32:
-    return ctypes.c_uint32(((x) >> 16) & IOCPARM_MASK)
+def iocbasecmd(x) -> __uint32_t:
+    return __uint32_t((x) & ~(IOCPARM_MASK << 16))
 
-def iocbasecmd(x) -> ctypes.c_uint32:
-    return ctypes.c_uint32((x) & ~(IOCPARM_MASK << 16))
-
-def iocgroup(x) -> ctypes.c_uint32:
-    return ctypes.c_uint32(((x) >> 8) & 0xff)
+def iocgroup(x) -> __uint32_t:
+    return __uint32_t(((x) >> 8) & 0xff)
 
 
-IOCPARM_MAX: ctypes.c_uint32 = ctypes.c_uint32(IOCPARM_MASK.value + 1) # max size of ioctl args
+IOCPARM_MAX: __uint32_t = __uint32_t(IOCPARM_MASK.value + 1) # max size of ioctl args
 # no parameters
-IOC_VOID: ctypes.c_uint32 = ctypes.c_uint32(0x20000000)
+IOC_VOID: __uint32_t = __uint32_t(0x20000000)
 # copy parameters out
-IOC_OUT: ctypes.c_uint32 = ctypes.c_uint32(0x40000000)
+IOC_OUT: __uint32_t = __uint32_t(0x40000000)
 # copy parameters in
-IOC_IN: ctypes.c_uint32 = ctypes.c_uint32(0x80000000)
+IOC_IN: __uint32_t = __uint32_t(0x80000000)
 # mask for IN/OUT/VOID
-IOC_INOUT = ctypes.c_uint32(IOC_IN.value|IOC_OUT.value)
+IOC_INOUT = __uint32_t(IOC_IN.value|IOC_OUT.value)
 
 
-def ioc(inout, group, num, len) -> ctypes.c_uint32:
-    return ctypes.c_uint32(inout.value | ((len & IOCPARM_MASK.value) << 16) | (ord(group) << 8) | (num))
+def ioc(inout, group, num, len) -> __uint32_t:
+    return __uint32_t(inout.value | ((len & IOCPARM_MASK.value) << 16) | (ord(group) << 8) | (num))
 
-def io(g, n) -> ctypes.c_uint32:
+def io(g, n) -> __uint32_t:
     return ioc(IOC_VOID, (g), (n), 0)
 
-def ior(g, n, t) -> ctypes.c_uint32:
+def ior(g, n, t) -> __uint32_t:
     return ioc(IOC_OUT, (g), (n), ctypes.sizeof(t))
 
-def iow(g, n, t) -> ctypes.c_uint32:
+def iow(g, n, t) -> __uint32_t:
     return ioc(IOC_IN, (g), (n), ctypes.sizeof(t))
 
 # this should be _IORW, but stdio got there first # but we dont got stdlib but to keep it consisnent we going with _IOWR
-def iowr(g, n, t) -> ctypes.c_uint32:
+def iowr(g, n, t) -> __uint32_t:
     return ioc(IOC_INOUT, (g), (n), ctypes.sizeof(t))
 
 
 def main():
     class ioctl_data(ctypes.Structure):
         _fields_ = [
-            ("x", ctypes.c_uint32)
+            ("x", __uint32_t)
         ]
 
-    iowr_test: ctypes.c_uint32 = iowr('M', 1, ioctl_data);
+    iowr_test: __uint32_t = iowr('M', 1, ioctl_data);
     print("iowr_test",iowr_test.value)
 
     io_test = io('M',2)
